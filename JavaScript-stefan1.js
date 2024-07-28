620efced38bb59f1c39d6862f169957109c74ee6
heitorjody <94492159+heitorjody@users.noreply.github.com>
2024-07-28 14:49:47 -0300

    Add files via upload

---
+++ JavaScript-stefan1.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const comissaoForm = document.getElementById('comissao-form');
+
+    comissaoForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const produto = document.getElementById('produto').value;
+
+        alert(`Pedido de Comissão Enviado!\nNome: ${nome}\nEmail: ${email}\nProduto: ${produto}`);
+
+        comissaoForm.reset();
+    });
+});
